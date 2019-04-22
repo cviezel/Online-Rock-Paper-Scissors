5ca1ca62fe9e7f916096d8266a4506cc8f2635da
Jasper Suhr <jsuhr2@binghamton.edu>
2019-04-22 10:49:42 -0400

    php for login and register works

--- register.php
+++ register.php
@@ -3,14 +3,14 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 #connecting
-$dbname = "jsuhr2_prs";
+$dbname = "jsuhr2_rps";
 $dbpassword = "aet5Aixoohai";
 
 $dbuser = "jsuhr2";
-$host = "mysql.cd.binghamton.edu";
+$host = "mysql.cs.binghamton.edu";
 $cid = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
 
-$sql = "select * from users wher username=\"" . addslashes($username) . "\"";
+$sql = "select * from users where username=\"" . addslashes($username) . "\"";
 $result  = mysqli_query($cid, $sql);
 $numRows = $result->num_rows;
 
